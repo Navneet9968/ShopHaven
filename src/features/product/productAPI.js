fb7b6d8b4c85bff76c308766cecbd584d6b7d8a9
Navneet9968 <navneetkumar9810@gmail.com>
2024-04-09 00:39:17 +0530

    Put the functionality to go to each product separately with added function of selectProductById in Slice and API and productDetail.js

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -7,27 +7,56 @@ export function fetchAllProducts() {
   });
 }
 
-export function fetchProductsByFilters(filter, sort) {
+export function fetchProductById(id) {
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8080/products/" + id);
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function fetchProductsByFilters(filter, sort, pagination) {
   //TODO : implement multi filter logic
   //filter = {category: ["smartphones","laptops"], brand: "Apple"}
   //sort = {_sort:"price",_order:"asc"}
+  //pagination={_page:1,_limit:10 }
+
   let queryString = "";
   for (let key in filter) {
     const categoryValues = filter[key];
-
     // For example, if filter was { color: 'red', size: 'large' },  queryString would be color=red&size=large&
     if (categoryValues.length > 0) {
       const lastCategoryValue = categoryValues[categoryValues.length - 1];
       queryString += `${key}=${lastCategoryValue}&`;
     }
   }
-
   for (let key in sort) {
+    queryString += `${key}=${sort[key]}&`;
+  }
+  for (let key in pagination) {
+    queryString += `${key}=${pagination[key]}&`;
   }
   return new Promise(async (resolve) => {
     const response = await fetch(
       "http://localhost:8080/products?" + queryString
     );
+    const data = await response.json();
+    const totalItems = response.headers.get("X-Total-Count");
+    console.log("totalItems", totalItems);
+    resolve({ data: { products: data, totalItems: totalItems } });
+  });
+}
+
+export function fetchCategories() {
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8080/categories");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+export function fetchBrands() {
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8080/brands");
     const data = await response.json();
     resolve({ data });
   });
